be7cd609b7a95524ab812eb3d33cefb073222ec4
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-10-25 09:31:47 +0200

    add: program logic and output done, slider added

--- README.md
+++ README.md
@@ -6,12 +6,12 @@
 
 - **MILESTONE 1**
 Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot.
-In allegato troverete markup con il codice html e css già fatti.
----
-- **MILESTONE 2**
 Rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
 Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+---
+- **MILESTONE 2**
+Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 ---
 - **BONUS:**
 Aggiungere il **ciclo infinito** del carosello. Ovvero se è attiva la prima immagine e l'utente clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
@@ -39,3 +39,5 @@ Aggiungere il **ciclo infinito** del carosello. Ovvero se è attiva la prima imm
 ## Anteprima
 
 ![preview](img/preview.JPG)
+
+
--- js/script.js
+++ js/script.js
@@ -1,50 +1,106 @@
 // RACCOLTA DATI, DICHIARAZIONE VARIABILI
-const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
-const itemsBlock = document.querySelector(".items");
-const item = document.createElement("div");
-item.classList.add("item");
-let imageItem = document.createElement("img");
-const display = [];
-// console.log(itemsBlock);
-// console.log(item,item.classList);
+const images = ["./img/01.jpg", "./img/02.jpg", "./img/03.jpg", "./img/04.jpg", "./img/05.jpg"];
 
 
-// LOGICA DEL PROGRAMMA E OUPUT
 for (let i = 0; i < images.length; i++) {
-
-    const image = images[i];
-    // console.log(imageItem);
-    imageItem.src = `${image}`;
-    // console.log(imageItem);
-
-    if (i === 0) {
-
-        item.classList.add("active");
-        display.push(true);
-
-    } else {
-
-        display.push(false)
-    }
-
-    item.append(imageItem);
-    // console.log(item);
-    itemsBlock.append(item);
-    console.log(display);
+    const curImage = images[i];
+    // console.log(curImage);
+    const items = document.querySelector(".items");
+    // console.log(items);
+    items.innerHTML += `<div class="item active">
+    <img src="${curImage}" alt="">
+    </div>`
 
 }
 
+const string = document.querySelectorAll(".item");
+console.log(string);
+
+let itemFlag;
+
+for (let i = 0; i < string.length; i++) {
+    curItem = string[i];
+    // console.log(curItem);
+    if (i != 0) {
+        curItem.classList.remove("active");
+        itemFlag = false;
+    } else {
+        itemFlag = true;
+    }
+
+}
+
+console.log(string);
+let string1 = string[0];
+let string2 = string[1];
+let string3 = string[2];
+let string4 = string[3];
+let string5 = string[4];
+
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
+let curPosition = 0;
+
+next.addEventListener("click", function () {
+    // console.log(itemFlag);
+    for (let i = 0; i < string.length - 1; ++i) {
+        curPosition = i + 1;
+        console.log(curPosition);
+        // console.log(nextImage);
+                
+        if (string[i].classList == "item active") {
+           
+            // console.log("ok");
+            // console.log("----");
+            string[i].classList.remove("active");
+            string[i + 1].classList.add("active");
+            // console.log(curPosition);
+
+            break
+            
+        } else if (string[4].classList == "item active") {
+            
+            console.log("wow");
+            string[4].classList.remove("active");
+            string[3].classList.remove("active");
+            string[2].classList.remove("active");
+            string[1].classList.remove("active");
+            string[0].classList.add("active");
+            
+            break
+
+        }
+
+    }
+})
+console.log(curPosition);
 
 prev.addEventListener("click", function () {
-    for (let i = 0; i < display.length; i++) {
-        const curImage = display[i];
-        const prevImage = images[i - 1];
-        console.log(curImage, prevImage);
-        if (curImage) {
-            console.log("ok");
-            // item.classList.remove("active");
+    for (let i = curPosition; i >= 0; i--) {
+        curPosition = i;
+        console.log(curPosition);
+        
+        if (string[i].classList == "item") {
+           
+            // console.log("ok");
+            // console.log("----");
+            string[i + 1].classList.remove("active");
+            string[i].classList.add("active");
+            
+            break
+            
+        } else if (string[0].classList != "item") {
+
+            console.log("wAw");
+            console.log(curPosition);
+            string[0].classList.remove("active");
+            // string[1].classList.add("active");
+            // string[2].classList.add("active");
+            // string[3].classList.add("active");
+            string[4].classList.add("active");
+            curPosition += 3;
+            console.log(curPosition);
+            
         }
 
     }
